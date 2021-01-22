6b72dcc7c6314eebcae5e6eb1e4fd2edb74ef8e7
AbdurRazzakFarhad <76758939+AbdurRazzakFarhad@users.noreply.github.com>
2021-01-22 19:38:25 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,69 @@
+// No:1
+function kilometerToMeter(km) {
+    if (km >= 0) {
+      var meter = km * 1000;
+      return meter;
+    } else {
+      return "Distance can't be negative!!";
+    }
+  }
+
+  
+
+// No:2
+  function budgetCalculator(Watch, Phone, Laptop) {
+    if ((Watch >= 0, Phone >= 0, Laptop >= 0)) {
+      var product1 = Watch * 50;
+      var product2 = Phone * 100;
+      var product3 = Laptop * 500;
+  
+      var totalPrice = product1 + product2 + product3;
+      return totalPrice;
+    } else {
+      return "Enter some positive value.";
+    }
+  }
+
+  
+  
+// No:3
+  function hotelCost(day) {
+    if (day >= 0) {
+      totalCost = 0;
+      if (day <= 10) {
+        var totalCost = day * 100;
+      } else if (day <= 20) {
+        var firstPart = 10 * 100;
+        var remains = day - 10;
+        var secondPart = remains * 80;
+        totalCost = firstPart + secondPart;
+      } else {
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remains = day - 20;
+        var thirdPart = remains * 50;
+        totalCost = firstPart + secondPart + thirdPart;
+      }
+      return totalCost;
+    } else {
+      return "Provide some valid data.";
+    }
+  }
+
+  
+
+// No:4
+  function megaFriend(arr) {
+    if (arr.length > 0) {
+      let longestName = " ";
+      for (var i = 0; i < arr.length; i++) {
+        if (longestName.length < arr[i].length) {
+          longestName = arr[i];
+        }
+      }
+      return longestName;
+    } else {
+      return "Empty arry isn't allowed.";
+    }
+  }
+  
